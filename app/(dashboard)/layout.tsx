5392f1cadfed6894d5e09968258f0dcc2423b590
Divine Enyinnayah <enyinnayahd@gmail.com>
2026-03-12 14:13:53 +0100

    rubbish

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,16 @@
 import { Sidebar } from "@/components/dashboard/sidebar"
+import type { Metadata } from 'next';
 
-const DashboardLayout = ({ children, }: { children: React.ReactNode} ) => {
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default function DashboardLayout({ children }: LayoutProps) {
     return (
         <div className="h-full">
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
